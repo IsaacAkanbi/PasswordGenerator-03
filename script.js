5034fa645d5edb0de3a3c42587f1ee05e76ecf01
IsaacAkanbi <isaac.akanbi@hotmail.co.uk>
2021-03-21 15:33:09 -0400

    updated JS script

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 //Assignment Code
 var generateBtn = document.querySelector("#generate");
+var possibleCharacter ="ABCDEFGHIJKLMNOPQRSUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(){}[]=<>/,.";
+
+function generatePassword()
+{
+  var length = window.prompt("How many characters do you want?");
+  if (length < 8 || length > 50) {
+    window.alert("Enter a valid number between 8 and 50");
+    var length = window.prompt("How many characters do you want?");
+  }
+  var storeNumber = confirm("Do you want to include number?");
+  var storeUpper = confirm("Do you want to include upper case alphabet?");
+  var storeLower = confirm("Do you want to include lower case alphabet?");
+  var storeSpecial = confirm("Do you want to include special character?");
+  while (
+    storeNumber === false &&
+    storeUpper === false &&
+    storeLower === false &&
+    storeSpecial === false
+  ) {
+    window.alert("You must select at least 1 character");
+    var storeNumber = confirm("Do you want to include number?");
+    var storeUpper = confirm("Do you want to include upper case alphabet?");
+    var storeLower = confirm("Do you want to include lower case alphabet?");
+    var storeSpecial = confirm("Do you want to include special character?");
+  }
+  
+  for (var i = 0; i < length; i++) {
+    randomPassword =
+      randomPassword +
+      possibleCharacter[Math.floor(Math.random() * possibleCharacter.length)];
+    console.log(randomPassword);
+  }
+  return randomPassword;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -7,8 +41,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+generateBtn.addEventListener("click", writePassword);
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
